aa31c6fe3818ecaee9abd0cc18ba1199ad032512
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2021-04-17 11:24:04 +0300

    Person model class is created.

---
+++ ETSWorkshop/Model/PersonData.swift
@@ -0,0 +1,17 @@
+//
+//  PersonData.swift
+//  ETSWorkshop
+//
+//  Created by Kaan Ozdemir on 16.04.2021.
+//
+
+import Foundation
+
+struct PersonData {
+    var name: String?
+    var surname: String?
+    var birthdatTimeStamp: TimeInterval?
+    var email: String?
+    var phoneNumber: String?
+    var note: String?
+}
